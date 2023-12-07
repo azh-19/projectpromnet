a1c28ef56b534cac04ea64e3ed739da2c59cb44a
azh-19 <azharnur183@gmail.com>
2023-12-07 10:56:28 +0700

    update azhar1

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+let signupBtn = document.getElementById("signupBtn");
+let loginBtn = document.getElementById("loginBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+let signupForm = document.getElementById("signupForm");
+
+loginBtn.onclick = function(){
+    nameField.style.maxHeight = "0";
+    title.innerHTML = "Log In";
+    signupBtn.classList.add("disable");
+    loginBtn.classList.remove("disable");
+}
+
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = "60px";
+    title.innerHTML = "Sign Up";
+    signupBtn.classList.remove("disable");
+    loginBtn.classList.add("disable");
+}
+
+signupForm.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+      window.location.href = 'index.html';
+  }
+});
+
+
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
@@ -21,6 +48,7 @@ var swiper = new Swiper(".home-slider", {
 } 
 
 window.addEventListener('load', init()); */
+
 
 window.onscroll = () =>{
   menu.classList.remove('fa-times');
